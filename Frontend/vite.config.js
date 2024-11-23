20595a4f7d8e75ea8c101a07eeebc3d6f4ec82fd
Briegal <brigalangom99@gmail.com>
2024-11-23 13:01:43 +0300

    added .env and vite file

---
+++ Frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'build', // specify the output directory for the build files
+        sourcemap: true, // optional: enable source maps for debugging production code
+    },
+    server: {
+        port: 3000, // default port for local development, can be changed
+    },
+    preview: {
+        port: 10000, // Port for previewing the build
+    },
+});
